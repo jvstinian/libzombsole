50fecf02af9ddc4161fdd6043fea47d2517b97f1
Justin Smith <jvstinian@gmail.com>
2024-08-14 08:52:28 -0400

    Cleaning up gym env.

--- zombsole/gym_env.py
+++ zombsole/gym_env.py
@@ -1,17 +1,13 @@
 #!/usr/bin/env python
 # coding: utf-8
 from os import path, system
-# import gym
 from gym.core import Env
 from gym.spaces import Box
 from gym.spaces.discrete import Discrete
-# from gym import error
-# from gym.utils import closer
 from zombsole.game import Game, Map
 import time
 import numpy as np
 
-# env_closer = closer.Closer()
 
 class ZombsoleGymEnv(object):
     """The main OpenAI Gym class. It encapsulates an environment with
@@ -32,18 +28,13 @@ class ZombsoleGymEnv(object):
         observation_space: The Space object corresponding to valid observations
         reward_range: A tuple corresponding to the min and max possible rewards
 
-    Note: a default reward range set to [-inf,+inf] already exists. Set it if you want a narrower range.
-
     The methods are accessed publicly as "step", "reset", etc...
     """
-    # Set this in SOME subclasses
     # See the supported modes in the render method
     metadata = {
         'render.modes': ['human']
     }
     reward_range = (-float('inf'), float('inf'))
-    # spec = None
-
     game_actions = [
         { 
             'action_type': 'move',
@@ -68,10 +59,9 @@ class ZombsoleGymEnv(object):
             'action_type': 'heal'
         }
     ]
-
     # Set these in ALL subclasses
     action_space = Discrete(len(game_actions))
-    # observation_space = Box(low=0, high=8*16*16, shape=(1, self.game.world.size[0], self.game.world.size[1]), dtype=np.int32)
+    # setting observation_space in the constructor
 
     def __init__(self, rules_name, player_names, map_name, agent_id, initial_zombies=0,
                  minimum_zombies=0, debug=False):
@@ -86,32 +76,14 @@ class ZombsoleGymEnv(object):
             agent_ids = [agent_id],
             debug=debug,
         )
-        # Does this work?
-        self.observation_space = Box(low=0, high=8*16*16, shape=(1, self.game.world.size[0], self.game.world.size[1]), dtype=np.int32)
+        self.observation_space = Box(low=0, high=8*16*16, shape=(1, self.game.world.size[1], self.game.world.size[0]), dtype=np.int32)
 
     def get_observation(self):
-        # TODO: make a method for retrieving state
-        # observation = {
-        #     'world': self.draw_world_simple(),
-        #     'ticks': self.game.world.t,
-        #     'deaths': self.game.world.deaths,
-        #     'players': [
-        #         (
-        #             player.name,
-        #             player.life,
-        #             player.position[0],
-        #             player.position[1],
-        #             player.weapon.name, # or 'unarmed'
-        #         ) for player in (self.players + self.agents)
-        #     ]
-        # }
-
         observation = np.array(self.game.encode_world_simple())
         return observation.reshape( (1,) + observation.shape )
     
     def get_frame_size(self):
         return tuple(reversed(self.game.map.size))
-        # return self.game.map.size
 
     def step(self, action):
         """Run one timestep of the environment's dynamics. When end of
@@ -132,7 +104,7 @@ class ZombsoleGymEnv(object):
         game_action = self.game_actions[action]
         self.game.agents[0].set_action(game_action)
 
-        frames_per_second=None #2.0
+        frames_per_second=None
 
         zombie_deaths_0 = self.game.world.zombie_deaths 
         # player_deaths_0 = self.game.world.player_deaths 
@@ -282,45 +254,6 @@ class ZombsoleGymEnv(object):
         self.close()
         # propagate exception
         return False
-
-
-# class GoalEnv(Env):
-#     """A goal-based environment. It functions just as any regular OpenAI Gym environment but it
-#     imposes a required structure on the observation_space. More concretely, the observation
-#     space is required to contain at least three elements, namely `observation`, `desired_goal`, and
-#     `achieved_goal`. Here, `desired_goal` specifies the goal that the agent should attempt to achieve.
-#     `achieved_goal` is the goal that it currently achieved instead. `observation` contains the
-#     actual observations of the environment as per usual.
-#     """
-
-#     def reset(self):
-#         # Enforce that each GoalEnv uses a Goal-compatible observation space.
-#         if not isinstance(self.observation_space, gym.spaces.Dict):
-#             raise error.Error('GoalEnv requires an observation space of type gym.spaces.Dict')
-#         for key in ['observation', 'achieved_goal', 'desired_goal']:
-#             if key not in self.observation_space.spaces:
-#                 raise error.Error('GoalEnv requires the "{}" key to be part of the observation dictionary.'.format(key))
-
-#     def compute_reward(self, achieved_goal, desired_goal, info):
-#         """Compute the step reward. This externalizes the reward function and makes
-#         it dependent on a desired goal and the one that was achieved. If you wish to include
-#         additional rewards that are independent of the goal, you can include the necessary values
-#         to derive it in 'info' and compute it accordingly.
-
-#         Args:
-#             achieved_goal (object): the goal that was achieved during execution
-#             desired_goal (object): the desired goal that we asked the agent to attempt to achieve
-#             info (dict): an info dictionary with additional information
-
-#         Returns:
-#             float: The reward that corresponds to the provided achieved goal w.r.t. to the desired
-#             goal. Note that the following should always hold true:
-
-#                 ob, reward, done, info = env.step()
-#                 assert reward == env.compute_reward(ob['achieved_goal'], ob['goal'], info)
-#         """
-#         raise NotImplementedError
-
 
 # class Wrapper(Env):
 #     """Wraps the environment to allow a modular transformation.
