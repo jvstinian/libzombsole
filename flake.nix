9224c4413c6d53e74d6af45792d0eaa8637962c1
jvstinian <jvstinian@gmail.com>
2024-10-06 00:23:41 -0400

    Basic test cases and flake configuration (#17)
    * Adding tests to the python package and configure the flake.

--- flake.nix
+++ flake.nix
@@ -42,8 +42,15 @@
             docopt termcolor pillow opencv4 gym
           ];
 
-          # Not including "nativeCheckInputs" as there are no additional dependencies for testing
           doCheck = true;
+          # Including pytestCheckHook in nativeCheckInputs to run pytest. 
+	  # If needed, arguments can be passed to pytest using pytestFlagsArray.  
+	  # Alternatively, checkPhase can be explicitly provided.
+	  # See https://github.com/NixOS/nixpkgs/blob/master/doc/languages-frameworks/python.section.md#using-pytestcheckhook 
+	  # for more details.
+          nativeCheckInputs = with pkgs.python310.pkgs; [
+            pytestCheckHook 
+          ];
       };
       my-python-packages = ps: with ps; [
           docopt
