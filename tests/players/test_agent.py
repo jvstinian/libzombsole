3f08df25f6f0067d75836ebd8f53dafe73eff6a5
Justin Smith <jvstinian@gmail.com>
2024-10-06 12:26:45 -0400

    Fixing agent test.

--- tests/players/test_agent.py
+++ tests/players/test_agent.py
@@ -6,5 +6,5 @@ from zombsole.weapons import Rifle
 def test_agent_icon_basic():
     agent = create("0", Rifle, None, objectives=None)
     assert agent.agent_id == "0"
-    assert agent.icon_basic == "P"
+    assert agent.icon_basic == "A"
 
