0db9111c1ab4542d944fee475ddba14b67478406
Justin Smith <jvstinian@gmail.com>
2024-11-16 10:44:32 -0500

    Clean up of rewards class.  We will not track player information such as health, only agent information.

--- zombsole/gym/reward.py
+++ zombsole/gym/reward.py
@@ -12,42 +12,27 @@ class AgentRewardsInterface(ABC):
 
 
 class AgentRewards(AgentRewardsInterface):
-    # class AgentState(object):
-    #     def __init__(self, agent):
-    #         self.alive = (agent.life > 0)
-    #         self.life = agent.life
-
-    #     def get_total_reward(self):
-    #         return (
-    #             self.life
-    #         )
-
-    def __init__(self, agents, world, game_end_value, include_life_in_reward=False, include_players_health=False):
+    def __init__(self, agents, world, game_end_value, include_life_in_reward=True):
         self.agents_life = [agent.life for agent in agents]
         self.zombie_deaths = 0
-        # self.players_health = game.get_players_health()
         self.game_end_value = game_end_value
         self.include_life_in_reward = include_life_in_reward
     
     def reset(self, agents, world):
         self.agents_life = [agent.life for agent in agents]
         self.zombie_deaths = 0
-        # self.players_health = game.get_players_health()
 
     def update(self, agents, world):
         prev_reward = self.get_total_reward()
         for idx in range(0, len(self.agents_life)):
             self.agents_life[idx] = agents[idx].life
         self.zombie_deaths = world.zombie_deaths
-        # self.players_health = game.get_players_health()
         return self.get_total_reward() - prev_reward
 
     def get_total_reward(self):
         reward = self.zombie_deaths
         if self.include_life_in_reward:
-            reward += sum(self.agents_life)
-        # if self.include_players_health:
-        #     reward += self.players_health
+            reward += sum(self.agents_life)/100.0
         return reward
 
     def get_game_end_reward(self, won: bool):
